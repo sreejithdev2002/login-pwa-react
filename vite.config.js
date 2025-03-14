3c019b5780b38f5d2447b4b226331a4ce422115a
Sreejith U <126107363+sreejithdev2002@users.noreply.github.com>
2025-03-14 16:14:51 +0530

    Made some change

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+
 const manifestForPlugIn = {
   registerType: "prompt",
-  includeAssets: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
   manifest: {
     id: "pwa-react-sreejithdev2002",
     name: "React-vite-app",
@@ -33,20 +34,17 @@ const manifestForPlugIn = {
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
-      },
+      }
     ],
     theme_color: "#171717",
     background_color: "#f0e7db",
     display: "fullscreen",
     scope: "/",
     start_url: "/",
-    orientation: "portrait",
-    launch_handler: {
-      client_mode: "focus-existing",
-    },
-  },
+    orientation: "portrait"
+  }
 };
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react(), VitePWA(manifestForPlugIn)],
 });
